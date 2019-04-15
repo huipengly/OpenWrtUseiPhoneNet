64b004d279b5591cda72dacb956a3eefb49da8e2
huipengly <huipengly@gmail.com>
2019-04-15 12:57:23 +0800

    Updata openwrt.sh for 18.06

--- openwrt.sh
+++ openwrt.sh
@@ -4,7 +4,7 @@
 
 #install packages
 opkg update
-opkg install kmod-usb-net-ipheth kmod-usb-net kmod-usb-ohci libimobiledevice-utils libplist-utils libusbmuxd-utils usbmuxd
+opkg install kmod-usb-net kmod-usb-net-cdc-ether kmod-usb-net-rndis kmod-usb-net-ipheth kmod-usb2 usbutils usbmuxd libusbmuxd libimobiledevice libimobiledevice-utils
 
 #usbmuxd auto-start
 cp usbmuxd /etc/init.d/usbmuxd
@@ -14,14 +14,12 @@ chmod +x /etc/init.d/usbmuxd
 /etc/init.d/usbmuxd start
 
 #set network
-uci delete network.wan
-
 uci set network.iPhone=interface
 uci set network.iPhone.proto=dhcp
 uci set network.iPhone.ifname=eth1
 uci commit network
 
-uci set firewall.@zone[1].network='wan6 iPhone'
+uci set firewall.@zone[1].network='wan wan6 iPhone'
 uci commit firewall
 /etc/init.d/firewall restart
 /etc/init.d/network reload
